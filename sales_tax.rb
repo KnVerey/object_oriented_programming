6b423a006040c7b31a0b64f3e194d059ca04f02f
Katrina Verey <kn.verey@gmail.com>
2013-10-23 15:42:04 -0400

    Started sales tax problem - input hashes

---
+++ sales_tax.rb
@@ -0,0 +1,19 @@
+
+
+input1 = {
+	"book" => 12.49,
+	"music" => 14.99,
+	"chocolate bar" => 0.85
+}
+
+input2 = {
+	"imported box of chocolates" => 10.00,
+	"imported bottle of perfume" => 47.50
+}
+
+input3 = {
+	"imported bottle of perfume" => 27.99,
+	"bottle of perfume" => 18.99,
+	"headache pills" => 9.75,
+	"box of imported chocolates" => 11.25
+}
