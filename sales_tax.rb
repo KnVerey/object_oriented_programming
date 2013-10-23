2754d21658c870e8958bdda38d13fcc0580e21ca
Katrina Verey <kn.verey@gmail.com>
2013-10-23 16:57:45 -0400

    tax rate finished

--- sales_tax.rb
+++ sales_tax.rb
@@ -5,44 +5,47 @@ class Goods
 	def initialize(item, price)
 		@item = item
 		@price = price
-		@tax = 0
 	end
 
 	def tax_rate
 		exempted = ["book", "chocolate", "pill"]
 
-		if exempted.any? {|exi| @item.include?(exi)}
-			puts "I'm exempt"
+		if @item.include?("imported")
+			return 15
+		elsif exempted.any? {|exi| @item.include?(exi)}
+			return 0
+		else
+			return 10
 		end
 	end
 
 	def tax
-		tax_rate
+		rate = tax_rate()
 	end
 end
 
 
-input1 = {
-	"book" => 12.49,
-	"music" => 14.99,
-	"chocolate bar" => 0.85
-}
+# input1 = {
+# 	"book" => 12.49,
+# 	"music" => 14.99,
+# 	"chocolate bar" => 0.85
+# }
 
 # input2 = {
 # 	"imported box of chocolates" => 10.00,
 # 	"imported bottle of perfume" => 47.50
 # }
 
-# input3 = {
-# 	"imported bottle of perfume" => 27.99,
-# 	"bottle of perfume" => 18.99,
-# 	"headache pills" => 9.75,
-# 	"box of imported chocolates" => 11.25
-# }
+input3 = {
+	"imported bottle of perfume" => 27.99,
+	"bottle of perfume" => 18.99,
+	"headache pills" => 9.75,
+	"box of imported chocolates" => 11.25
+}
 
 list = []
 counter = 0
-input1.each do |item, price|
+input3.each do |item, price|
 	list[counter] = Goods.new(item, (price*100).to_i)
 	counter += 1
 end
