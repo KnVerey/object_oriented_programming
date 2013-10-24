a278dc5aa40d7c0f751b1d38f0e68237d6f2ff54
Katrina Verey <kn.verey@gmail.com>
2013-10-24 00:40:51 -0400

    Finished receipt output.

--- sales_tax.rb
+++ sales_tax.rb
@@ -1,19 +1,30 @@
 
 class Bill
-	attr_accessor :products
+	attr_accessor :products, :total_tax
 	
 	def initialize(input)
 		@products = []
 		counter = 0
 		input.each do |name, price, quantity|
 			@products[counter] = Item.new(name, (price*100).to_i, quantity)
-		counter += 1
+			counter += 1
 		end
+
+		@total_tax = sum_tax()
+	end
+
+	def sum_tax
+		@products.inject(0) {|sum, item| sum+item.tax}.to_f/100
+	end
+
+	def sum_total
+		(@products.inject(0)	{|sum, item| sum+item.price}.to_f/100) + @total_tax
 	end
 
 	def print_receipt
 		@products.each {|item| item.print_item}
-
+		puts "Sales Taxes: $#{@total_tax}"
+		puts "Total: $#{sum_total}"
 	end
 
 end	
@@ -61,17 +72,23 @@ input1 = [
 	["chocolate bar", 0.85, 1]
 ]
 
-# input2 = [
-# 	["imported box of chocolates", 10.00, 1],
-# 	["imported bottle of perfume", 47.50, 1]
-# ]
+input2 = [
+	["imported box of chocolates", 10.00, 1],
+	["imported bottle of perfume", 47.50, 1]
+]
 
-# input3 = [
-# 	["imported bottle of perfume", 27.99, 1],
-# 	["bottle of perfume", 18.99, 1],
-# 	["headache pills", 9.75, 1],
-# 	["box of imported chocolates", 11.25, 1]
-# ]
+input3 = [
+	["imported bottle of perfume", 27.99, 1],
+	["bottle of perfume", 18.99, 1],
+	["headache pills", 9.75, 1],
+	["box of imported chocolates", 11.25, 1]
+]
 
-puts "Bill 1:"
+puts "\nBill 1:"
 Bill.new(input1).print_receipt
+
+puts "\nBill 2:"
+Bill.new(input2).print_receipt
+
+puts "\nBill 3:"
+Bill.new(input3).print_receipt
