b381e2fec54d3d4829cabaed9cf759489b7ae650
Katrina Verey <kn.verey@gmail.com>
2013-10-23 22:34:12 -0400

    changed to arrays to add quantity

--- sales_tax.rb
+++ sales_tax.rb
@@ -1,10 +1,11 @@
 
 class Goods
-	attr_accessor :item, :price, :tax
+	attr_accessor :item, :price, :quantity, :tax
 
-	def initialize(item, price)
+	def initialize(item, price, quantity)
 		@item = item
 		@price = price
+		@quantity = quantity
 		@tax = tax()
 	end
 
@@ -24,38 +25,38 @@ class Goods
 
 	def tax
 		rate = tax_rate()
-		return (rate*@price/100).round(-1)
+		return (rate*@quantity*@price/100).round(-1)
 	end
 
 	def receipt
-		puts "#{@item}: #{(@price.to_f+@tax.to_f)/100}"
+		puts "#{@quantity} #{@item}: #{(@price.to_f*quantity+@tax.to_f)/100}"
 	end
 end
 
 
-# input1 = {
-# 	"book" => 12.49,
-# 	"music" => 14.99,
-# 	"chocolate bar" => 0.85
-# }
+# input1 = [
+# 	"book", 12.49, 1],
+# 	"music", 14.99, 1],
+# 	"chocolate bar", 0.85, 1]
+# ]
 
-# input2 = {
-# 	"imported box of chocolates" => 10.00,
-# 	"imported bottle of perfume" => 47.50
-# }
+# input2 = [
+# 	"imported box of chocolates", 10.00, 1],
+# 	"imported bottle of perfume", 47.50, 1]
+# ]
 
-input3 = {
-	"imported bottle of perfume" => 27.99,
-	"bottle of perfume" => 18.99,
-	"headache pills" => 9.75,
-	"box of imported chocolates" => 11.25
-}
+input3 = [
+	["imported bottle of perfume", 27.99, 1],
+	["bottle of perfume", 18.99, 1],
+	["headache pills", 9.75, 1],
+	["box of imported chocolates", 11.25, 1]
+]
 
 list = []
 counter = 0
-input3.each do |item, price|
-	list[counter] = Goods.new(item, (price*100).to_i)
-	counter += 1
+input3.each do |item, price, quantity|
+		list[counter] = Goods.new(item, (price*100).to_i, quantity)
+		counter += 1
 end
 
 list.each {|x| x.receipt}
